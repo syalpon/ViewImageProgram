f9fb2cedabc47ff3ea5d13ac2b6019e1e9d81cc6
syalpon <onoue0107@gmail.com>
2021-04-17 18:00:00 +0900

    プロジェクト ファイルを追加します。

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.IO;
+using ViewImageProgram;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var size = 10;
+            Square shape = new Square(size);
+            Assert.AreEqual(shape.Height, 10);
+            Assert.AreEqual(shape.Width , 10);
+        }
+    }
+}
---
+++ ViewImageProgram/Square.cs
@@ -0,0 +1,28 @@
+﻿namespace ViewImageProgram
+{
+    public class Square
+    {
+        /*フィールド*/
+        protected int _width ; // X size
+        protected int _height; // Y size
+
+        /*プロパティ*/
+        public int Width
+        {
+            set { _width = value; }
+            get { return _width; }
+        }
+
+        public int Height
+        {
+            set { _height = value; }
+            get { return _height; }
+        }
+        /*コンストラクタ*/
+        public Square(int size)
+        {
+            _width  = size;
+            _height = size;
+        }
+    }
+}
